4bd4a09b47d8fd35660ed9f5b75a20f3ccbf99b6
Ander <ikagil25rlhusurbil.eus>
2026-02-25 12:03:33 +0100

    login orria egiten

---
+++ Web/htdocs/js/Login.js
@@ -0,0 +1,16 @@
+function balidatu() {
+    const emailInput = document.getElementById("emaila").value;
+    const passwordInput = document.getElementById("pasahitza").value;
+
+    const garbiMsg = document.getElementById("garbi");
+    const erroreaMsg = document.getElementById("errorea");
+
+    garbiMsg.classList.add("ezkutuan");
+    erroreaMsg.classList.add("ezkutuan");
+
+    if (emailInput === "IndiUsurbil@admin.com" && passwordInput === "IndiUsurbilAdmin") {
+        garbiMsg.classList.remove("ezkutuan");
+    } else {
+        erroreaMsg.classList.remove("ezkutuan");
+    }
+}
